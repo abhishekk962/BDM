ce7dfb0ff5c2d567b7e840dfe2e887b23d445c62
Abhishek Kumar <abhishekk962@gmail.com>
2024-12-23 21:13:12 -0500

    Added dialog guide for app use

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pydeck as pdk
 import pandas as pd
+from streamlit.components.v1 import html
 
 # Set the page layout to wide mode
 st.set_page_config(layout="wide")
@@ -87,7 +88,6 @@ st.pydeck_chart(r, use_container_width=True)
 with open("style.css") as f:
     st.markdown('<style>{}</style>'.format(f.read()), unsafe_allow_html=True)
 
-
 col1, col2 = st.columns(2)
 
 with col1:
@@ -106,7 +106,7 @@ with col1:
         chart_data = chart_data[(chart_data["brands"] == selected_brand)]
     chart_data = chart_data.groupby("hour_of_day")["counts"].sum().reset_index()
 
-    st.line_chart(chart_data, y='counts', x='hour_of_day', x_label="Hour of Day", y_label="Visits", use_container_width=True, color='#FF0000')
+    st.line_chart(chart_data, y='counts', x='hour_of_day', x_label="Hour of Day", y_label="Visits", use_container_width=True, color='#ff4b4b', height=250)
 
 with col2:
     # Create a chart for the visits & discounts by store
@@ -117,7 +117,37 @@ with col2:
     if selected_brand:
         chart2_data = chart2_data[(chart2_data["brands"] == selected_brand)]
         chart2_data = chart2_data.groupby("street_address")["counts"].sum().reset_index()
-        st.bar_chart(chart2_data, x='street_address', y='counts', x_label="Store", y_label="Visits", use_container_width=True, color='#FF0000')
+        st.bar_chart(chart2_data, x='street_address', y='counts', y_label="Store", x_label="Visits", use_container_width=True, color='#ff4b4b', height=250, horizontal=True)
     else:
         chart2_data = chart2_data.groupby("brands")["counts"].sum().reset_index()
-        st.bar_chart(chart2_data, x='brands', y=['counts'], x_label="Brand", y_label="Visits", use_container_width=True, color='#FF0000')
+        chart2_data["brands"] = chart2_data["brands"].str.split("(").str[0]
+        st.bar_chart(chart2_data, x='brands', y=['counts'], y_label="Brand", x_label="Visits", use_container_width=True, color='#ff4b4b', height=250, horizontal=True)
+
+
+@st.dialog(title="SuperShopper")
+def show_dialog(num):
+    st.write(messages[num])
+    if st.button(buttons[num]):
+        st.session_state.show_dialog = {num: True}
+        st.rerun()
+
+messages = ["Welcome to SuperShopper! Plan your Black Friday & Cyber Monday shopping to avoid missing out on deals and skip the long lines!",
+"Browse your favorite Brands, Stores, and Discounts in the left pane.",
+"Check out the bottom bar for all the Black Friday and Cyber Monday days!",
+"The right pane helps you choose the best times and places to shop—avoid the crowds!",
+"Use the map to find nearby stores and start your shopping spree! Happy Shopping!"]
+
+buttons = ["Great!", "Got it!", "Sounds good!", "That's great!", "Let's go!"]
+
+if "show_dialog" not in  st.session_state:
+    show_dialog(0)
+else:
+    if 0 in st.session_state.show_dialog:
+        show_dialog(1)
+    elif 1 in st.session_state.show_dialog:
+        show_dialog(2)
+    elif 2 in st.session_state.show_dialog:
+        show_dialog(3)
+    elif 3 in st.session_state.show_dialog:
+        show_dialog(4)
+
